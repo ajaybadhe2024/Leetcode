fd86b6115b29f9f360084cb9b4601a40d72ffa6f
ajaybadhe2024 <ajaybadhe2024@gmail.com>
2026-08-24 18:45:01 +0530

    Time: 11 ms (91.31%) | Memory: 45.8 MB (38.48%) - LeetSync

--- 917-boats-to-save-people/boats-to-save-people.cpp
+++ 917-boats-to-save-people/boats-to-save-people.cpp
@@ -6,20 +6,16 @@ public:
         int i=0;
         int j=n-1;
         int boat=0;
-      
         while(i<=j){
             if((people[i]+people[j])<=limit){
                 boat++;
                 i++;
                 j--;
-            
             }else{
                 boat++;
                 j--;
-     
             }
         }
         return boat;
-        
     }
 };
