8880ded3c37307c68e4da20f0523a2ea8e20590c
ajaybadhe2024 <ajaybadhe2024@gmail.com>
2026-08-25 18:54:54 +0530

    Time: 4 ms (42.40%) | Memory: 18.5 MB (48.57%) - LeetSync

--- 23-merge-k-sorted-lists/merge-k-sorted-lists.cpp
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.cpp
@@ -10,27 +10,24 @@
  */
 class Solution {
 public:
+    ListNode* mergetwosort(ListNode* l1,ListNode* l2){
+        if(l1==NULL){
+            return l2;
+        }
+        if(l2==NULL){
+            return l1;
+        }
 
-ListNode* mergetwosort(ListNode* l1,ListNode* l2){
-    if(l1==NULL){
-        return l2;
+        if(l1->val<=l2->val){
+            l1->next=mergetwosort(l1->next,l2);
+            return l1;
+        }else{
+            l2->next=mergetwosort(l2->next,l1);
+            return l2;
+        }
+
     }
-    if(l2==NULL){
-        return l1;
-
-    }
-
-    if(l1->val<=l2->val){
-        l1->next=mergetwosort(l1->next,l2);
-        return l1;
-    }else{
-        l2->next=mergetwosort(l2->next,l1);
-        return l2;
-    }
-
-
-}
-   ListNode* partialmergelist(int s,int e,vector<ListNode*>& lists){
+   ListNode* partialmerge(int s,int e,vector<ListNode*>& lists){
     if(s>e){
         return NULL;
     }
@@ -38,8 +35,8 @@ ListNode* mergetwosort(ListNode* l1,ListNode* l2){
         return lists[s];
     }
     int mid=s+(e-s)/2;
-    ListNode* l1=partialmergelist(s,mid,lists);
-    ListNode* l2=partialmergelist(mid+1,e,lists);
+    ListNode* l1=partialmerge(s,mid,lists);
+    ListNode* l2=partialmerge(mid+1,e,lists);
     return mergetwosort(l1,l2);
    }
     ListNode* mergeKLists(vector<ListNode*>& lists) {
@@ -47,8 +44,8 @@ ListNode* mergetwosort(ListNode* l1,ListNode* l2){
         if(k==0){
             return NULL;
         }
-     return partialmergelist(0,k-1,lists);
-        
+
+        return  partialmerge(0,k-1,lists);
         
     }
 };
